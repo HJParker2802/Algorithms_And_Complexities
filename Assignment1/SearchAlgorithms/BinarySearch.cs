f76839d94b178c701fe4e182da1bf7ea5055fece
HJParker2802 <125221273+HJParker2802@users.noreply.github.com>
2025-03-26 16:47:00 +0000

    Updated BinarySearch
    Added commenting and updated variable names to fit code style

--- Assignment1/SearchAlgorithms/BinarySearch.cs
+++ Assignment1/SearchAlgorithms/BinarySearch.cs
@@ -13,88 +13,94 @@ namespace Assignment1.Search
     class BinarySearch : Searching
     {
         public List<int> Result = new List<int>();
-        public int closestValue = -1;
-        public int closestIndex = -1;
-        public int closestDiff = int.MaxValue;
-        public override List<int> SearchKey(List<int> Array,int start, int stop, int Key)
+        public int ClosestValue = -1;
+        public int ClosestIndex = -1;
+        public int ClosestDiff = int.MaxValue;
+
+        public override List<int> SearchKey(List<int> Array,int Start, int Stop, int Key)
         {
             if (!(Array.Contains(Key)))
             {
                 OutOfBounds = true;
-                return SearchForClosestValue(Array, start, stop, Key);
+                return SearchForClosestValue(Array, Start, Stop, Key);//if key not found in array, find closest value 
             }
             else
             {
-                if (start > stop) return Result;
-                int mid = (start + stop) / 2;
-                int CheckValue = Array[mid];
-                if (Key == CheckValue)
-                {
-                    Result.Add(mid);  
+                if (Start > Stop) return Result;//if start > stop, code cannot correctly run 
 
-                    int left = mid - 1;
+                int Mid = (Start + Stop) / 2;//Calculate Middle Index of Array
+                int CheckValue = Array[Mid];//Value at Middle Index
+                
+
+                if (Key == CheckValue)//If key == CheckValue, match found
+                {
+                    Result.Add(Mid);  
+
+                    //Check for duplicates on left of middle element
+                    int left = Mid - 1;
                     while (left >= 0 && Array[left] == Key)
                     {
                         Result.Add(left);
-                        left--;
+                        left--;//move to next index on left
                     }
 
-                    int right = mid + 1;
+                    //Check for duplicates on right of middle element
+                    int right = Mid + 1;
                     while (right < Array.Count && Array[right] == Key)
                     {
                         Result.Add(right);
-                        right++;
+                        right++;//Move to next index on right 
                     }
                 }
 
                 else
                 {
-                    if (Key < CheckValue)
+                    if (Key < CheckValue)//if key is smaller than checkvalue, search left half of array
                     {
-                        return SearchKey(Array, start, mid - 1, Key);
+                        return SearchKey(Array, Start, Mid - 1, Key);
                     }
-                    else
+                    else//if key is larger than checkvalue, search right half of array 
                     {
-                        return SearchKey(Array, mid + 1, stop, Key);
+                        return SearchKey(Array, Mid +1, Stop, Key);
                     }
                 }
             }
             if (OutOfBounds)
             {
-                Result.Add(closestIndex); 
+                Result.Add(ClosestIndex); //Ensures that if no key is found, the closest value is found and returned 
             }
             return Result;
-
         }
-        public override List<int> SearchForClosestValue(List<int> Array, int start, int stop, int Key)
+        public override List<int> SearchForClosestValue(List<int> Array, int Start, int Stop, int Key)
         {
-            if (start > stop)
+            if (Start > Stop)//if start > stop, code cannot correctly run so return result
             {
-                if (closestValue != -1)
+                if (ClosestValue != -1)//if closest index found, add it
                 {
-                    Result.Add(closestIndex);
+                    Result.Add(ClosestIndex);//adds closest index
                 }
                 return Result;
             }
 
-            int mid = (start + stop) / 2;
-            int CheckValue = Array[mid];
+            int Mid = (Start + Stop) / 2;//Calculate Middle Index of Array
+            int CheckValue = Array[Mid];//Value at Middle Index
 
-            int currentDiff = Math.Abs(CheckValue - Key);
-            if (currentDiff < closestDiff)
+            int CurrentDiff = Math.Abs(CheckValue - Key);//Checks for absolute difference to ensure positive or negative difference is ignored
+            if (CurrentDiff < ClosestDiff)//if current difference smaller than closest difference,
+                                          //update closeset diff, value and index 
             {
-                closestDiff = currentDiff;
-                closestValue = CheckValue;
-                closestIndex = mid;
+                ClosestDiff = CurrentDiff;
+                ClosestValue = CheckValue;
+                ClosestIndex = Mid;
             }
 
-            if (Key < CheckValue)
+            if (Key < CheckValue)//if key smaller than current middle, search left half of array
             {
-                return SearchForClosestValue(Array, start, mid - 1, Key);
+                return SearchForClosestValue(Array, Start, Mid - 1, Key);
             }
-            else
+            else//if key larger than current middle, search left half of array
             {
-                return SearchForClosestValue(Array, mid + 1, stop, Key);
+                return SearchForClosestValue(Array, Mid + 1, Stop, Key);
             }
         }
     }
